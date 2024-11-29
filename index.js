8b9eb1937b125764177a251de88faf9964d965c9
Jason Xie <jasonluxie@gmail.com>
2024-11-29 18:15:44 +0800

    added logic for fetch request to worker and worker parsing but currently stuck on reading request.body

--- index.js
+++ index.js
@@ -2,26 +2,34 @@ const responseElement = document.getElementById("response");
 const worker = "https://note-taker-kv.jasonluxie.workers.dev/";
 
 const userLogin = () => {
-    const userName = document.getElementById("username");
+    fetch(worker, {
+        method: "POST",
+        user: "test",
+        body: { user: "test", trigger: "load" },
+    })
+        .then((response) => response.text())
+        .then((data) => {
+            console.log(data);
+            responseElement.innerHTML = data;
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 };
 
 const submitAndUpdateList = (event) => {
-  event.preventDefault()
+    event.preventDefault();
     if (event.target.id === "to-do-submit") {
         let formValue = document.getElementById("to-do-entry").value;
-        fetch(worker, { method: "POST", body: formValue });
-        document.getElementById("to-do-entry").value = ""
+        fetch(worker, {
+            method: "POST",
+            user: "test",
+            trigger: "add",
+            body: {value: formValue, user:'test', trigger:'add'},
+        });
+        document.getElementById("to-do-entry").value = "";
     }
 };
 
+userLogin();
 responseElement.addEventListener("click", submitAndUpdateList);
-
-fetch(worker)
-    .then((response) => response.text())
-    .then((data) => {
-        console.log(data);
-        responseElement.innerHTML = data;
-    })
-    .catch((error) => {
-        console.error(error);
-    });
--- worker.js
+++ worker.js
@@ -1,15 +1,62 @@
 export default {
     async fetch(request, env, ctx) {
-        try {
-            let testKV = await env.toDoDictionary.put("test", request.body);
-            console.log(await env.toDoDictionary.get("test"));
-        } catch (error) {
-            console.error(error);
-        }
         console.log(request);
-        const toDoForm = `<form><input type="text" id="to-do-entry"/><input type="submit" id='to-do-submit' value="Add"/></form>`;
-        const response = new Response(toDoForm);
-        response.headers.set("Access-Control-Allow-Origin", "*"); // Change this to deployment domain later
-        return response;
+        const toDoInput = `<form><input type="text" id="to-do-entry"/><input type="submit" id='to-do-submit' value="Add"/></form>`;
+        if (request.body.trigger === "load") {
+            try {
+                const userToDo = await env.toDoDictionary.get(
+                    request.body.user
+                );
+                console.log(typeof JSON.parse(userToDo));
+                if (userToDo) {
+                    let list = "";
+                    for (let i = 0; i < userToDo.length; i++) {
+                        const listItem = `<li><p id=${i}>${userToDo[i]}</p></li>`;
+                        list = list + listItem;
+                    }
+                    console.log(list);
+                    const response = new Response(toDoInput + list);
+                    response.headers.set("Access-Control-Allow-Origin", "*"); //change wildcard to domain later
+                    response.headers.set(
+                        "Access-Control-Allow-Methods",
+                        "GET, POST"
+                    );
+                    response.headers.set(
+                        "Access-Control-Allow-Headers",
+                        "Content-Type"
+                    );
+                    return response;
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        } else if (request.body.trigger === "add") {
+            try {
+                let testKV = await env.toDoDictionary.put(
+                    "test",
+                    request.body.value
+                );
+                console.log(await env.toDoDictionary.get("test"));
+            } catch (error) {
+                console.error(error);
+            }
+            const response = new Response(toDoInput);
+            response.headers.set("Access-Control-Allow-Origin", "*");
+            response.headers.set("Access-Control-Allow-Methods", "GET, POST");
+            response.headers.set(
+                "Access-Control-Allow-Headers",
+                "Content-Type"
+            );
+            return response;
+        } else {
+            const response = "no entry";
+            response.headers.set("Access-Control-Allow-Origin", "*");
+            response.headers.set("Access-Control-Allow-Methods", "GET, POST");
+            response.headers.set(
+                "Access-Control-Allow-Headers",
+                "Content-Type"
+            );
+            return response;
+        }
     },
 };
