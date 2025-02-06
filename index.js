e58ba4fcbe6db4d681b3425889b7b440c0ed9a3a
Jason Xie <jasonluxie@gmail.com>
2025-02-05 19:30:20 -0600

    cors fixed in worker.js, page javascript fetch requests changed to test fetch methods

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
 const responseElement = document.getElementById("response");
-const worker = "https://note-taker-kv.jasonluxie.workers.dev/";
+const worker = "https://serverless-kv-to-do-list.jasonluxie.workers.dev";
 
-const peageLoad = () => {
-    fetch(worker, {
-        method: "PUT",
-        body: JSON.stringify({ user: "test", trigger: "load" }),
+const requestHeaders = {
+  Origin: "http://127.0.0.1:5500/",
+  "Access-Control-Request-Method": "GET, PUT, POST",
+  "Access-Control-Request-Headers": "Content-Type",
+};
+
+const getFetch = () => {
+  fetch(worker, {
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      console.log(data);
     })
-        .then((response) => 
-            response.text()
-        )
-        .then((data) => {
-            console.log(data);
-            responseElement.innerHTML = data;
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
-const submitAndUpdateList = (event) => {
-    event.preventDefault();
-    if (event.target.id === "to-do-submit") {
-        let formValue = document.getElementById("to-do-entry").value;
-        fetch(worker, {
-            method: "PUT",
-            body: { value: formValue, user: "test", trigger: "add" },
-        });
-        document.getElementById("to-do-entry").value = "";
-    }
+const putFetch = () => {
+  fetch(worker, {
+    method: "PUT",
+    body: JSON.stringify({ user: "test", trigger: "load" }),
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      console.log(data);
+      responseElement.innerHTML = data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
-peageLoad();
-responseElement.addEventListener("click", submitAndUpdateList);
+const pageLoad = () => {
+  getFetch();
+  putFetch();
+};
+
+// const submitAndUpdateList = (event) => {
+//     event.preventDefault();
+//     if (event.target.id === "to-do-submit") {
+//         let formValue = document.getElementById("to-do-entry").value;
+//         fetch(worker, {
+//             method: "PUT",
+//             body: { value: formValue, user: "test", trigger: "add" },
+//         });
+//         document.getElementById("to-do-entry").value = "";
+//     }
+// };
+
+pageLoad();
+// responseElement.addEventListener("click", submitAndUpdateList);
--- worker.js
+++ worker.js
@@ -1,31 +1,46 @@
 export default {
-    async fetch(request, env, ctx) {
-        const setKV = (user, data) => env.toDoDictionary.put(user, data);
-        const getKV = (user) => env.toDoDictionary.get(user);
-        // const toDoInput = `<form><input type="text" id="to-do-entry"/><input type="submit" id='to-do-submit' value="Add"/></form>`;
-        const body = await request.text();
-        try {
-            //const parsed = JSON.parse(body)
-            if(getKV(parsed.user)) {
-                getKV(parsed.user).forEach(element => {
-                    //make html for each item in array
-                });
-            }
-            const response = new Response(body, { status: 200 });
-            response.headers.set(
-                "Access-Control-Allow-Origin",
-                "http://127.0.0.1:5500"
-            );
-            response.headers.set("Access-Control-Allow-Methods", "PUT");
-            return response;
-        } catch (error) {
-            const response = new Response(error, { status: 500 });
-            response.headers.set(
-                "Access-Control-Allow-Origin",
-                "http://127.0.0.1:5500"
-            );
-            response.headers.set("Access-Control-Allow-Methods", "PUT");
-            return response;
-        }
-    },
+  async fetch(request, env, ctx) {
+    const setKV = (user, data) => env.toDoDictionary.put(user, data);
+    const getKV = (user) => env.toDoDictionary.get(user);
+    // const toDoInput = `<form><input type="text" id="to-do-entry"/><input type="submit" id='to-do-submit' value="Add"/></form>`;
+    // const body = await request.text();
+
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": "http://127.0.0.1:5500/", // Change to deployment website later
+      "Access-Control-Allow-Methods": "GET,PUT,POST,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    };
+
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        headers: {
+          ...corsHeaders,
+          "Access-Control-Max-Age": "86400",
+        },
+      });
+    }
+    // Handle the actual request logic
+    if (request.method === "GET") {
+      return new Response("GET", {
+        status: 200,
+        headers: {
+          ...corsHeaders,
+        },
+      });
+    } else if (request.method === "POST") {
+      return new Response("POST", {
+        status: 200,
+        headers: {
+          ...corsHeaders,
+        },
+      });
+    } else if (request.method === "PUT") {
+      return new Response("PUT", {
+        status: 200,
+        headers: {
+          ...corsHeaders,
+        },
+      });
+    }
+  },
 };
